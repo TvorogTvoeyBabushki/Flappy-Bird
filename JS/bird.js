223afc1cb9281057580dc09cdba6c45993ef955e
Grimes <snake3773@mail.ru>
2023-04-21 18:40:06 +0300

    feat: add bird and background animation

---
+++ JS/bird.js
@@ -0,0 +1,6 @@
+export default class Bird {
+    constructor() {
+        this.imgBird = new Image()
+        this.imgBird.src = './'
+    }
+}
--- JS/canvas.js
+++ JS/canvas.js
@@ -3,11 +3,28 @@ export default class Canvas {
         this.element = document.getElementById('flappy-bird')
         this.context = this.element.getContext('2d')
 
+        this.element.width = 288
+        this.element.height = 630
+
         this.background = new Image()
-        this.background.src = './Images/background.png'
+        this.background.src = 'Images/background.png'
+
+        this.foreground = new Image()
+        this.foreground.src = 'Images/foreground.png'
+
+        this.index = 0
+        this.speed = 1
+        this.backgroundX = 0
     }
 
     draw() {
-        this.context.drawImage(this.background, 0, 0)
+        this.index++
+        this.backgroundX = -((this.index * this.speed) % this.element.width)
+
+        this.context.drawImage(this.background, this.backgroundX, 0)
+        this.context.drawImage(this.background, this.backgroundX + this.element.width, 0)
+
+        this.context.drawImage(this.foreground, this.backgroundX, this.background.height)
+        this.context.drawImage(this.foreground, this.backgroundX + this.element.width, this.background.height)
     }
 }
